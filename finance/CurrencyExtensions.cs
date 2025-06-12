50b30fcc9b02ad514f61fbb0f06a1231962f9c3d
Switch <31280229+gavin1970@users.noreply.github.com>
2025-06-12 18:46:02 -0400

    Visual update and document link and version update

--- finance/CurrencyExtensions.cs
+++ finance/CurrencyExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Chizl.RegexPatterns.utils;
+using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
@@ -7,8 +8,9 @@ namespace Chizl.RegexPatterns.Finance
     internal static class CurrencyExtensions
     {
         private static Dictionary<Enum, CurrencyAttribute> _currencyAttribute = new Dictionary<Enum, CurrencyAttribute>();
-        public static CurrencyAttribute CurrencyAttr(this Currency @this) => GetAttribute(@this);
         public static bool DropAttributes<T>(this T @this) where T : Enum => _currencyAttribute.Remove(@this);
+
+        public static CurrencyAttribute CurrencyAttr(this Currency @this) => GetAttribute(@this);
         public static string CurrencyName(this Currency @this) => GetAttribute(@this).CurrencyName;
         public static string CountryName(this Currency @this) => GetAttribute(@this).CountryName;
         public static bool HasMinorUnits(this Currency @this) => GetAttribute(@this).HasMinorUnits;
@@ -19,19 +21,21 @@ namespace Chizl.RegexPatterns.Finance
                 currencySymbol = Regex.Escape(currencySymbol);
             return currencySymbol;
         }
+
         public static bool IsRequired(this UnitStatus @this) => @this.Equals(UnitStatus.Required);
         public static bool IsOptional(this UnitStatus @this) => @this.Equals(UnitStatus.Optional);
         public static bool IsDenied(this UnitStatus @this) => @this.Equals(UnitStatus.Deny);
         public static bool IsRequired(this DisplayStatus @this) => @this.Equals(DisplayStatus.Required);
         public static bool IsOptional(this DisplayStatus @this) => @this.Equals(DisplayStatus.Optional);
         public static bool IsDenied(this DisplayStatus @this) => @this.Equals(DisplayStatus.Deny);
-        private static CurrencyAttribute GetAttribute(Enum enumVal)
+
+        private static CurrencyAttribute GetAttribute(Currency enumVal)
         {
             if (_currencyAttribute.TryGetValue(enumVal, out CurrencyAttribute attrib))
                 return attrib;
 
             var type = enumVal.GetType();
-            var memInfo = type.GetMember($"{enumVal}");
+            var memInfo = type.GetMember(enumVal.Name());
             var attributes = memInfo[0].GetCustomAttributes(typeof(CurrencyAttribute), false);
             var retVal = (attributes.Length > 0) ? (CurrencyAttribute)attributes[0] : null;
 
